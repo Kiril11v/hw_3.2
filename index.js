8b8df06b42a398478fa1074a2c622d297bb64de0
Kyrylo_Kud <kudkiril@gmail.com>
2025-06-18 22:31:26 +0200

    hw_3.2

---
+++ index.js
@@ -0,0 +1,71 @@
+
+    let message = '';
+    const PITY_MESSAGE = "It's a pity you didn't enter ";
+    const noEntered = "nothing is entered";
+    const MAX_YEAR = 100;
+    const MIN_YEAR = 1;
+    const MAX_LENGTH = 15;
+    const MIN_LENGTH = 2;
+
+    const userName = prompt("your name:");
+    if (userName === null) {
+        message += PITY_MESSAGE + 'your name';
+    }
+        else if (!userName.trim()) {
+            message += noEntered;
+        }
+        else if (!isNaN(userName) || +userName >= MAX_LENGTH || +userName < MIN_LENGTH) {
+          message += "error: invalid name";
+        }
+    else {
+        const name = userName.trim(); 
+        message +=  `Your name is ${name}`; 
+    }
+
+    message += '\n';
+
+    const userAge = prompt("your age:");
+    if (userAge === null) {
+        message +=PITY_MESSAGE + 'your age';
+    }
+    else if (!userAge.trim()) {
+            message += noEntered;
+        }
+    else if (!Number.isInteger(+userAge) || +userAge >= MAX_YEAR || +userAge < MIN_YEAR) {
+        message +="error: invalid age";
+    }
+    else {
+        const age = userAge.trim();
+        message += `You're ${age} old`;
+    }
+
+    message += '\n';
+
+while(true) {
+    if (confirm("enter OK if you a man")) {
+  message += "You are a man";
+  break;
+} 
+else {
+  if (confirm("enter OK if you a woman")) {
+  message += "You are a woman";
+  break;
+} 
+else {
+  message += `${PITY_MESSAGE} your gender`;
+  break;
+}
+ }
+
+}
+
+
+alert (message);
+
+
+    
+    
+
+
+
+
